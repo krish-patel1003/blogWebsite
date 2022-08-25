4dc28f8285b28a1c0c6015dee40fffbbb172b3f6
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2022-08-25 19:20:44 +0530

    changed url

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('https://mellifluous-malasada-7053f0.netlify.app/', include('blog.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
